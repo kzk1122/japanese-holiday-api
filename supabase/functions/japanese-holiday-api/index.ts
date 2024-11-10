d94e0b19de25314cf4e69f6b601847883cdbe736
kazuki takahashi <kazuthankyoukzk@gmail.com>
2024-11-10 20:58:45 +0900

    エラーハンドリング周りをFunctionに切り出し、全体的な調整

--- supabase/functions/japanese-holiday-api/index.ts
+++ supabase/functions/japanese-holiday-api/index.ts
@@ -9,39 +9,50 @@ type HolidayInfoResponse = {
   holidays: HolidayInfo[];
 }
 
+type ErrorInfo = {
+  message: string;
+}
+
 // 環境変数からデータベース接続URLを取得
-const databaseUrl = Deno.env.get('SUPABASE_DB_URL')!
+const DATABASE_URL = Deno.env.get('SUPABASE_DB_URL')!
+// 環境変数から接続プールのサイズを取得
+const CONNECTION_POOL_SIZE = Number(Deno.env.get('CONNECTION_POOL_SIZE') ?? 3); 
 
 // コネクションプールを作成
-const pool = new postgres.Pool(databaseUrl, 3, true)
+const pool = new postgres.Pool(DATABASE_URL, CONNECTION_POOL_SIZE, true);
+
+// エラーレスポンスを返却
+function buildErrorResponse(message: string, status: number): Response {
+  const errorMessage: ErrorInfo = { message };
+  return new Response(JSON.stringify(errorMessage), { status });
+}
 
 Deno.serve(async (req) => {
   try {
-
-    const { url, method } = req
+    const { url, method } = req;
 
     // GET以外のリクエストは拒否
     if (method !== 'GET') {
-      return new Response('Method Not Allowed', { status: 405 })
+      return buildErrorResponse('Method Not Allowed.', 405);
     }
 
     // pathparamの取得
-    const yearPattern = new URLPattern({ pathname: '/japanese-holiday-api/:year' })
-    const matchingPath = yearPattern.exec(url)
-    const year = matchingPath ? matchingPath.pathname.groups.year : null
+    const yearPattern = new URLPattern({ pathname: '/japanese-holiday-api/:year' });
+    const matchingPath = yearPattern.exec(url);
+    const year = matchingPath ? matchingPath.pathname.groups.year : null;
 
     // yearが形式通りセットされていない場合はエラー
     if (!year) {
-      return new Response('Year is not set properly', { status: 400 })
+      return buildErrorResponse('Year is not set properly.', 400);
     }
 
-    // Numberに変換できない場合はエラー
-    if (isNaN(Number(year))) {
-      return new Response('Year is not set properly', { status: 400 })
+    const yearRegex = /^\d{4}$/; // YYYY形式
+    if (!yearRegex.test(year)) {
+      return buildErrorResponse('The year is not in the YYYY format.', 400);
     }
 
     // コネクションプールから接続を取得する
-    const connection = await pool.connect()
+    const connection = await pool.connect();
 
     try {
       // DBから対象の祝日を取得
@@ -64,13 +75,13 @@ Deno.serve(async (req) => {
       return new Response(JSON.stringify(response), {
         status: 200,
         headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      })
+      });
     } finally {
       // コネクションを解放
-      connection.release()
+      connection.release();
     }
   } catch (error) {
-    console.error(error)
-    return new Response(String(error?.message ?? error), { status: 500 })
+    console.error(error);
+    return buildErrorResponse(`Internal Server Error.`, 500);
   }
 })
